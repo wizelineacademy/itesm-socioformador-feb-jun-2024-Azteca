b7debea359f18f6746a28939809e75a13232d057
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-04-09 14:42:19 -0600

    Solved Profile View

---
+++ components/ProjectsCarousel.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import ProjectCard from "@/components/ProjectCard";
+import Autoplay from "embla-carousel-autoplay";
+
+export default function EmblaCarousel() {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, []);
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <ul className="embla__container">
+        <li className="embla__slide__project">
+          <ProjectCard />
+        </li>
+        <li className="embla__slide__project">
+          <ProjectCard />
+        </li>
+        <li className="embla__slide__project">
+          <ProjectCard />
+        </li>
+        <li className="embla__slide__project">
+          <ProjectCard />
+        </li>
+        <li className="embla__slide__project">
+          <ProjectCard />
+        </li>
+      </ul>
+    </div>
+  );
+}
