aaa6f4abe034e05a1efaf45a71ae59df16914f10
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-04-10 15:30:59 -0600

    Created PlusIcon

--- components/ProjectExtendedCard.tsx
+++ components/ProjectExtendedCard.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import UserProfileButton from "@/components/UserProfileButton";
 import ChevronRightIcon from "./icons/ChevronRightIcon";
+import PlusIcon from "./icons/PlusIcon";
 
 const ProjectExtendedCard = () => {
   const pathname = usePathname();
@@ -22,27 +23,7 @@ const ProjectExtendedCard = () => {
             <UserProfileButton size="xs" className="mx-[-6px]" />
             <UserProfileButton size="xs" className="mx-[-6px]" />
             <UserProfileButton size="xs" className="mx-[-6px]" />
-            <button
-              disabled
-              className={
-                "group mx-[-6px] rounded-full bg-white p-2 text-primary drop-shadow-lg"
-              }
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2.5}
-                stroke="currentColor"
-                className="h-4 w-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 4.5v15m7.5-7.5h-15"
-                />
-              </svg>
-            </button>
+            <PlusIcon size="h-4 w-4" color="primary" />
           </div>
           <ChevronRightIcon path="/projects/1" currentPath={pathname} />
         </div>
