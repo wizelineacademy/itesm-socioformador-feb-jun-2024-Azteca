9acd30fca27ec5485b2e4448d4120cd103eec6bd
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-04-11 15:26:16 -0600

    Created Project Creation Page Base

--- app/projects/create/page.tsx
+++ app/projects/create/page.tsx
@@ -1,9 +1,76 @@
+"use client";
+
 import React from "react";
 
 const CreateProject = () => {
   return (
-    <div>
-      <h3>Project Creation</h3>
+    <div className="mx-28 my-6">
+      <h3 className="text-4xl font-medium">Project Creation</h3>
+      <form action="" className="mt-4">
+        {/* Title */}
+        <div className="my-4 flex flex-col">
+          <label htmlFor="title" className="text-2xl font-medium">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            placeholder="Enter a title for your project"
+            className="mt-2 rounded-xl p-2 drop-shadow-lg"
+          />
+        </div>
+
+        <div className="my-4 flex">
+          <div>
+            {/* Project Duration */}
+            <div className="flex flex-col">
+              <label htmlFor="duration" className="text-2xl font-medium">
+                Duration
+              </label>
+              <div className="flex gap-8">
+                <input type="text" id="start" />
+                <input type="date" id="end" />
+              </div>
+            </div>
+
+            {/* Sprint Survey Periodicity */}
+            <div className="my-4 flex flex-col">
+              <label htmlFor="periodicity" className="text-2xl font-medium">
+                Sprint Survey Periodicity
+              </label>
+              <select name="" id="periodicity">
+                <option value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+              </select>
+            </div>
+          </div>
+
+          {/* Description */}
+          <div className="flex flex-col">
+            <label htmlFor="description" className="text-2xl font-medium">
+              Description
+            </label>
+            <textarea name="" id="description" cols={30} rows={10}></textarea>
+          </div>
+        </div>
+
+        <div className="flex justify-between">
+          {/* Members */}
+          <div className="my-4 flex flex-col">
+            <label htmlFor="members" className="text-2xl font-medium">
+              Members
+            </label>
+            <button>Add</button>
+          </div>
+
+          {/* Action Buttons */}
+          <div className="my-4 flex">
+            <button>Cancel</button>
+            <button>Create</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import ProjectExtendedCard from "@/components/ProjectExtendedCard";
 
 const Projects = () => {
@@ -7,9 +8,12 @@ const Projects = () => {
       <section className="mt-12 flex flex-col">
         <div className="mb-8 flex justify-center gap-5">
           <h3 className="text-4xl font-medium">My Projects</h3>
-          <button className="rounded-full bg-primary px-5 py-2 font-medium text-white drop-shadow-lg">
-            Create
-          </button>
+          <Link
+            href="/projects/create"
+            className="rounded-full bg-primary px-5 py-2 font-medium text-white drop-shadow-lg"
+          >
+            <button className="">Create</button>
+          </Link>
         </div>
         <div className="mx-auto flex max-w-[60%] flex-wrap items-center justify-center gap-6">
           <ProjectExtendedCard />
