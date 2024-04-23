61a0e906b5e5bc11f4836dcb77c34b5358ba301e
pedroalonsoms <pedro123ben10@gmail.com>
2024-04-22 21:16:57 -0600

    add sst deploy workflow

--- sst.config.ts
+++ sst.config.ts
@@ -1,25 +1,14 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-///No cambiar nada de este archivo, es el archivo de configuración de SST
-//putazos al que lo haga
+
 export default $config({
   app(input) {
     return {
-      name: "CodigoAztecaAppvf2",
+      name: "FeedbackFlow",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
   },
-
   async run() {
-    const database = new sst.aws.Postgres("FeedbackFlowdb7", {
-      scaling: {
-        min: "1 ACU",
-        max: "1 ACU",
-      },
-    });
-
-    new sst.aws.Nextjs("MyWeb5", {
-      link: [database],
-    });
+    new sst.aws.Nextjs("Nextjs");
   },
 });
