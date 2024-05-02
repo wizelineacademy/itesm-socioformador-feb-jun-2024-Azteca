72572288065533d66d9b9b12750d5831331fb479
José Carlos Sánchez <a01174050@tec.mx>
2024-05-02 15:25:16 -0600

    added photoUrls to surveys

--- components/modals/DraggableUser.tsx
+++ components/modals/DraggableUser.tsx
@@ -4,29 +4,35 @@ import Draggable from "../Draggable";
 import Tooltip from "../Tooltip";
 import { useState } from "react";
 import { SurveyCoworker } from "@/types";
+import UserProfileButton from "../UserProfileButton";
 
 const DraggableUser = ({ user }: { user: SurveyCoworker }) => {
   const [isDragging, setIsDragging] = useState(false);
-  //TODO: In case of providing a photoUrl, show the photo insted of the UserIcon:
+
   return (
     <Draggable id={user.userId} data={user} setIsDragging={setIsDragging}>
       {isDragging ? (
-        <UserIcon
-          size="w-10 h-10"
-          color={"text-blue-300" || "text-primary"}
-          className=" animate-[spin_1.5s_infinite]  rounded-full bg-white transition-all duration-100"
-        />
+        user.photoUrl ? (
+          <UserProfileButton photoUrl={user.photoUrl} size="sm" className="" />
+        ) : (
+          <UserIcon
+            size="w-10 h-10"
+            color="text-blue-300"
+            className="infinite animate-[spin_1.5s_infinite] rounded-full bg-white duration-100"
+          />
+        )
       ) : (
         <Tooltip message={user.name}>
           {user.times > 1 && (
-            <span className="relative left-3 top-3 flex h-3 w-3 cursor-move items-center justify-center rounded-full bg-primary-light p-2 text-[0.6rem] font-medium text-white ">
+            <span className="relative left-3 top-3 z-10 flex h-3 w-3 cursor-move items-center justify-center rounded-full bg-primary-light p-2 text-[0.6rem] font-medium text-white">
               x{user.times}
             </span>
           )}
-          <UserIcon
-            size="w-10 h-10"
-            color={"text-blue-300" || "text-primary"}
-          />
+          {user.photoUrl ? (
+            <UserProfileButton photoUrl={user.photoUrl} size="sm" />
+          ) : (
+            <UserIcon size="w-10 h-10" color="text-blue-300" />
+          )}
         </Tooltip>
       )}
     </Draggable>
