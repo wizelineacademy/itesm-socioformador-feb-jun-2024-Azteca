b4d3ae011065d892e19c8a120596fcbedb7bc089
AlexMendozaPrado <a00819383@tec.mx>
2024-05-03 08:14:27 -0600

    cambios

--- sst.config.ts
+++ sst.config.ts
@@ -11,12 +11,14 @@ export default $config({
   //
   async run() {
     const POSTGRES_URL = new sst.Secret("PostgresUrl");
-    const POSTGRES_PRISMA_URL = new sst.Secret("PostgresPrismaUrl");
+    const POSTGRES_PRISMA_URL = new sst.Secret("");
     const POSTGRES_USER = new sst.Secret("PostgresUser");
     const POSTGRES_HOST = new sst.Secret("PostgresHost");
     const POSTGRES_PASSWORD = new sst.Secret("PostgresPassword");
     const POSTGRES_DATABASE = new sst.Secret("PostgresDatabase");
     const AUTH_SECRET = new sst.Secret("AuthSecret");
+    const POSTGRES_URL_NO_SSL = new sst.Secret("PostgresUrlNoSsl");
+    const POSTGRES_URL_NON_POOLING = new sst.Secret("PostgresUrlNonPooling");
 
     new sst.aws.Nextjs("Nextjs1", {
       link: [
@@ -27,6 +29,8 @@ export default $config({
         POSTGRES_PASSWORD,
         POSTGRES_DATABASE,
         AUTH_SECRET,
+        POSTGRES_URL_NO_SSL,
+        POSTGRES_URL_NON_POOLING,
       ],
       environment: {
         POSTGRES_URL: POSTGRES_URL.value,
@@ -36,6 +40,8 @@ export default $config({
         POSTGRES_PASSWORD: POSTGRES_PASSWORD.value,
         POSTGRES_DATABASE: POSTGRES_DATABASE.value,
         AUTH_SECRET: AUTH_SECRET.value,
+        POSTGRES_URL_NO_SSL: POSTGRES_URL_NO_SSL.value,
+        POSTGRES_URL_NON_POOLING: POSTGRES_URL_NON_POOLING.value,
       },
     });
   },
