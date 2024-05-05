d87efc92f7b43188ba6db7681e728c9e7a9ccfe1
AlexMendozaPrado <a00819383@tec.mx>
2024-05-05 01:13:59 -0600

    cambios drizzle

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,6 +1,49 @@
+// Cargar variables de entorno desde .env
+require("dotenv").config();
+
+// Importaciones
+import { Resource } from "sst";
+//import { drizzle } from "drizzle-orm/aws-data-api/pg";
+import { migrate } from "drizzle-orm/aws-data-api/pg/migrator";
+import { RDSDataClient } from "@aws-sdk/client-rds-data";
+import { fromIni } from "@aws-sdk/credential-providers";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 
 const db = drizzle(sql);
 
 export default db;
+
+// Obtener el perfil de AWS desde las variables de entorno o usar 'default'
+const awsProfile = process.env.AWS_PROFILE || "default";
+
+// Configuración del cliente RDSDataClient usando el perfil de AWS
+const sql = new RDSDataClient({
+credentials: fromIni({ profile: awsProfile }),
+region: "us-east-1",
+});
+
+//console.log("SQL", sql);
+
+// Configuración de la conexión de Drizzle ORM
+export const db = drizzle(sql, {
+    database: Resource.FeedbackFlowdb.database,
+    secretArn: Resource.FeedbackFlowdb.secretArn,
+    resourceArn: Resource.FeedbackFlowdb.clusterArn,
+    });
+
+//console.log("DB", db);
+
+// Función principal para ejecutar migraciones
+const main = async () => {
+try {
+await migrate(db, { migrationsFolder: "drizzlemigrations" });
+console.log("Migration complete");
+} catch (error) {
+// console.log(error, "Error running migrations");
+ }
+process.exit(0);
+};
+
+// Ejecutar la función principal
+//main();
