5427bbd01c34ce139b5f169c06a802dd06a6f1c0
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-05-14 14:30:36 -0600

    Added Banner Selection

--- components/modals/Settings.tsx
+++ components/modals/Settings.tsx
@@ -12,15 +12,29 @@ import {
 import { Dropzone, IMAGE_MIME_TYPE, FileWithPath } from "@mantine/dropzone";
 import SunIcon from "../icons/SunIcon";
 import MoonIcon from "../icons/MoonIcon";
+import NextImage from "next/image";
+import Banner1 from "@/public/Banner1.svg";
+import Banner2 from "@/public/Banner2.svg";
+import Banner3 from "@/public/Banner3.svg";
+import Banner4 from "@/public/Banner4.svg";
 
 interface SettingsProps {
   showModal: boolean;
   onClose: () => void;
 }
+
+const images = [Banner1, Banner2, Banner3, Banner4];
+
 const Settings = ({ showModal, onClose }: SettingsProps) => {
   const [file, setFile] = useState<FileWithPath[]>([]);
   const [color, setColor] = useState("#6640D5");
   const { setColorScheme, clearColorScheme } = useMantineColorScheme();
+  const [selectedImage, setSelectedImage] = useState<number | null>(null);
+
+  const handleImageClick = (index: number) => {
+    setSelectedImage(index);
+    console.log(selectedImage);
+  };
 
   const preview = file.map((image, index) => {
     const imageUrl = URL.createObjectURL(image);
@@ -152,7 +166,39 @@ const Settings = ({ showModal, onClose }: SettingsProps) => {
                   </Tabs.Panel>
 
                   <Tabs.Panel value="bannerImage">
-                    Messages tab content
+                    <div className="container mx-auto p-4">
+                      <p className="mb-2 text-grayText">Select an Image</p>
+                      <div className="flex space-x-4">
+                        {images.map((src, index) => (
+                          <div
+                            key={index}
+                            className={`rounded border-2 ${selectedImage === index ? "border-primary" : "border-transparent"}`}
+                            onClick={() => handleImageClick(index)}
+                          >
+                            <NextImage
+                              src={src}
+                              alt={`Banner ${index + 1}`}
+                              className="cursor-pointer"
+                              width={200} // specify the width of the image
+                              height={200} // specify the height of the image
+                              layout="responsive" // layout optimization for responsiveness
+                            />
+                          </div>
+                        ))}
+                      </div>
+                      <div className="mt-2 flex justify-center">
+                        <button
+                          disabled={selectedImage === null}
+                          className={`${
+                            selectedImage === null
+                              ? "bg-gray-300"
+                              : "bg-primary hover:bg-primary-dark"
+                          } mt-4 rounded-lg px-10 py-2 font-medium text-white drop-shadow-lg`}
+                        >
+                          Upload
+                        </button>
+                      </div>
+                    </div>
                   </Tabs.Panel>
 
                   <Tabs.Panel value="colorTheme">
