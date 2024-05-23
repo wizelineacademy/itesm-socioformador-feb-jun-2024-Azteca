2441d4c6a8c67a82c223a33dc23392d6639d8127
José Carlos Sánchez <a01174050@tec.mx>
2024-05-23 09:51:40 -0600

    profile page hotfix

--- app/(base)/profile/[id]/page.tsx
+++ app/(base)/profile/[id]/page.tsx
@@ -24,20 +24,10 @@ const Profile: React.FC<{ params: { id: string } }> = async ({ params }) => {
 
         <section className="w-100 mx-auto flex justify-between space-x-10">
           <div className="w-7/12">
-            <ProfileSection
-              title="Co-workers"
-              showMore={true}
-              userId={params.id}
-              type="coworkers"
-            >
+            <ProfileSection title="Co-workers" showMore={true}>
               <CoWorkersCarousel userId={params.id} />
             </ProfileSection>
-            <ProfileSection
-              title="Projects"
-              showMore={true}
-              userId={params.id}
-              type="projects"
-            >
+            <ProfileSection title="Projects" showMore={true}>
               <ProjectsCarousel userId={params.id} />
             </ProfileSection>
           </div>
@@ -54,6 +44,32 @@ const Profile: React.FC<{ params: { id: string } }> = async ({ params }) => {
             />
           </div>
         </section>
+        {/*
+          TODO: @adrian and @jose are going to fix this
+
+          <div className="mx-auto flex justify-between">
+            <h3 className="text-2xl font-medium text-black">Strengths</h3>
+            <p className="cursor-pointer self-center text-sm text-graySubtitle">
+              Show More
+            </p>
+          </div>
+          <div
+            className={
+              traits.strengths.length === 0
+                ? "mb-6 mt-3 flex flex-wrap items-center justify-center gap-5 rounded-lg bg-slate-300/20 py-4"
+                : "mb-10 mt-5 flex flex-wrap gap-5"
+            }
+          >
+            {traits.strengths.length === 0 && (
+              <NoDataCard text="No strengths available yet" />
+            )}
+            {traits.strengths.map((strength, index) => (
+              <Tooltip message={strength.description || ""} key={index}>
+                <Badge text={strength.name || ""} />
+              </Tooltip>
+            ))}
+          </div>
+          */}
       </Suspense>
     </main>
   );
