87bfbad2898532381bfb66d5b67acf45a61fac5e
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-05-23 10:09:00 -0600

    Corrected ComboBoxOpen

--- app/(base)/profile/[id]/page.tsx
+++ app/(base)/profile/[id]/page.tsx
@@ -24,10 +24,20 @@ const Profile: React.FC<{ params: { id: string } }> = async ({ params }) => {
 
         <section className="w-100 mx-auto flex justify-between space-x-10">
           <div className="w-7/12">
-            <ProfileSection title="Co-workers" showMore={true}>
+            <ProfileSection
+              title="Co-workers"
+              showMore={true}
+              userId={params.id}
+              type="coworkers"
+            >
               <CoWorkersCarousel userId={params.id} />
             </ProfileSection>
-            <ProfileSection title="Projects" showMore={true}>
+            <ProfileSection
+              title="Projects"
+              showMore={true}
+              userId={params.id}
+              type="projects"
+            >
               <ProjectsCarousel userId={params.id} />
             </ProfileSection>
           </div>
@@ -42,9 +52,8 @@ const Profile: React.FC<{ params: { id: string } }> = async ({ params }) => {
               traits={traits.areasOfOportunity}
               emptyMessage="No areas of opportunity available yet"
             />
-          </div>
-        </section>
-        {/*
+
+            {/*
           TODO: @adrian and @jose are going to fix this
 
           <div className="mx-auto flex justify-between">
@@ -69,7 +78,33 @@ const Profile: React.FC<{ params: { id: string } }> = async ({ params }) => {
               </Tooltip>
             ))}
           </div>
-          */}
+
+          <div className="mx-auto flex justify-between">
+            <h3 className="text-2xl font-medium text-black">
+              Opportunity Areas
+            </h3>
+            <p className="cursor-pointer self-center text-sm text-graySubtitle">
+              Show More
+            </p>
+          </div>
+          <div
+            className={
+              traits.areasOfOportunity.length === 0
+                ? "mb-6 mt-3 flex flex-wrap justify-center gap-5 rounded-lg bg-slate-300/20 py-4"
+                : "mb-10 mt-5 flex flex-wrap gap-5"
+            }
+          >
+            {traits.areasOfOportunity.length === 0 && (
+              <NoDataCard text="No areas of opportunity available yet" />
+            )}
+            {traits.areasOfOportunity.map((area, index) => (
+              <Tooltip message={area.description || ""} key={index}>
+                <Badge text={area.name || ""} />
+              </Tooltip>
+            ))}
+          </div> */}
+          </div>
+        </section>
       </Suspense>
     </main>
   );
