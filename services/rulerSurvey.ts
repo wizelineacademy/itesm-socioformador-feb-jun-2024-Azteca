d862c250288a5b2190fe2222082d37c6df5261e0
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-05-28 13:44:25 -0600

    Fixed Emotions Summary Backend

--- services/rulerSurvey.ts
+++ services/rulerSurvey.ts
@@ -4,8 +4,16 @@ import { rulerSurveyAnswers } from "@/db/schema";
 import { sql } from "drizzle-orm";
 
 import { RulerSurveyAnswer } from "@/types/types";
+import { auth } from "@/auth";
 
 export async function submitRulerSurveyAnswer(surveyAnswer: RulerSurveyAnswer) {
+  if (!surveyAnswer.userId || surveyAnswer.userId === undefined) {
+    const session = await auth();
+    surveyAnswer.userId = session?.user?.id;
+    if (!surveyAnswer.userId) {
+      throw new Error("You most be signed in");
+    }
+  }
   // Insert ruler answer to db
   await db.insert(rulerSurveyAnswers).values({
     userId: surveyAnswer.userId,
