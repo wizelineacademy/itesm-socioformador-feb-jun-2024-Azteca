4729c9ac4577ad7461a68e61ded506e72ff5b5ae
Felipe Acosta <felipegonac0@gmail.com>
2024-06-04 23:53:35 -0600

    fix: notifications working bitch

--- services/notifications.ts
+++ services/notifications.ts
@@ -11,7 +11,7 @@ import {
   sprintSurvey,
   sprintSurveyAnswerProject,
 } from "@/db/schema";
-import { eq, and, sql, gte } from "drizzle-orm";
+import { eq, and, sql, gte, isNull } from "drizzle-orm";
 import { Notification } from "@/types/types";
 
 export async function getNotifications() {
@@ -21,15 +21,15 @@ export async function getNotifications() {
 
   const notifications = [];
 
-  const currentTimestamp = new Date(
-    new Date().toLocaleString("es-MX", { timeZone: "America/Monterrey" }),
-  );
+  // const currentTimestamp = new Date(
+  //   new Date().toLocaleString("es-MX", { timeZone: "America/Monterrey" }),
+  // );
 
-  const currentDateString = currentTimestamp.toISOString().split("T")[0];
-  const currentDateTimeString = currentTimestamp
-    .toISOString()
-    .replace("T", " ")
-    .split(".")[0];
+  // const currentDateString = currentTimestamp.toISOString().split("T")[0];
+  // const currentDateTimeString = currentTimestamp
+  //   .toISOString()
+  //   .replace("T", " ")
+  //   .split(".")[0];
 
   // Get the notifications of the sprint surveys
   const sprintSurveys = await db
@@ -37,65 +37,67 @@ export async function getNotifications() {
       id: sprintSurvey.id,
       projectName: project.name,
       date: sprintSurvey.scheduledAt,
-      answerUserId: sprintSurveyAnswerProject.userId,
     })
     .from(sprintSurvey)
     .innerJoin(project, eq(project.id, sprintSurvey.projectId))
     .innerJoin(projectMember, eq(projectMember.projectId, project.id))
     .leftJoin(
       sprintSurveyAnswerProject,
-      and(eq(sprintSurvey.id, sprintSurveyAnswerProject.sprintSurveyId)),
+      and(
+        eq(sprintSurvey.id, sprintSurveyAnswerProject.sprintSurveyId),
+        eq(sprintSurveyAnswerProject.userId, projectMember.userId),
+      ),
     )
     .where(
       and(
         eq(projectMember.userId, userId), // the surveys belong to a user's project
-        eq(sprintSurveyAnswerProject.userId, userId),
+        isNull(sprintSurveyAnswerProject.sprintSurveyId),
         eq(sprintSurvey.processed, false), // the surveys aren't processed
-        gte(sql`${currentDateTimeString}`, sprintSurvey.scheduledAt), // the survey itself is active (i.e. has been scheduled)
+        gte(sql`CURRENT_TIMESTAMP`, sprintSurvey.scheduledAt), // the survey itself is active (i.e. has been scheduled)
       ),
     );
+
   if (sprintSurveys.length > 0) {
     for (const sprintSurvey of sprintSurveys) {
-      if (!(sprintSurvey.answerUserId === userId)) {
-        notifications.push({
-          ...sprintSurvey,
-          type: "SPRINT",
-        });
-      }
+      notifications.push({
+        ...sprintSurvey,
+        type: "SPRINT",
+      });
     }
   }
-
   // Get the notifications of the final surveys
   const finalSurveys = await db
     .select({
       id: finalSurvey.id,
       projectName: project.name,
       date: finalSurvey.scheduledAt,
-      answerUserId: finalSurveyAnswer.userId,
     })
     .from(finalSurvey)
     .innerJoin(project, eq(project.id, finalSurvey.projectId))
     .innerJoin(projectMember, eq(projectMember.projectId, project.id))
     .leftJoin(
       finalSurveyAnswer,
-      and(eq(finalSurvey.id, finalSurveyAnswer.finalSurveyId)),
+      and(
+        eq(finalSurvey.id, finalSurveyAnswer.finalSurveyId),
+        eq(finalSurveyAnswer.userId, projectMember.userId),
+      ),
     )
     .where(
       and(
         eq(projectMember.userId, userId), // the surveys belong to a user's project
+        isNull(finalSurveyAnswer.finalSurveyId),
         eq(finalSurvey.processed, false), // the surveys aren't processed
-        gte(sql`${currentDateTimeString}`, finalSurvey.scheduledAt), // the survey itself is active (i.e. has been scheduled)
+        gte(sql`CURRENT_TIMESTAMP`, finalSurvey.scheduledAt), // the survey itself is active (i.e. has been scheduled)
       ),
     );
 
+  console.log("FINAL SURVEY: ", finalSurveys);
   if (finalSurveys.length > 0) {
     for (const finalSurvey of finalSurveys) {
-      if (!(finalSurvey.answerUserId === userId)) {
-        notifications.push({
-          ...finalSurvey,
-          type: "FINAL",
-        });
-      }
+      notifications.push({
+        ...finalSurvey,
+        type: "FINAL",
+      });
     }
   }
 
@@ -106,9 +108,11 @@ export async function getNotifications() {
     .where(
       and(
         eq(rulerSurveyAnswers.userId, userId), // the survey belongs to the user
-        eq(rulerSurveyAnswers.answeredAt, sql`${currentDateString}`), // is today's survey
+        eq(rulerSurveyAnswers.answeredAt, sql`CURRENT_TIMESTAMP::date`), // is today's survey
       ),
     );
+
+  console.log("RULER :", rulerSurveys);
 
   if (rulerSurveys.length === 0) {
     // if there are no answers, send notification
