d012a4249f824d9a24a7a713e172042c167efce2
José Carlos Sánchez <a01174050@tec.mx>
2024-06-07 12:23:36 -0600

    fix: hotfix of pcp select

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -5,7 +5,7 @@ interface InterfaceProgressBar {
 
 const ProgressBar = ({ width, height }: InterfaceProgressBar) => {
   return (
-    <div className="w-full rounded-full bg-gray-200 p-1.5">
+    <div className="w-3/4 rounded-full bg-gray-200 p-1.5">
       <div
         className={`h-${height} rounded-full bg-gradient-to-r from-primary to-secondary transition-all delay-100 ease-linear`}
         style={{ width: `${width}%` }}
