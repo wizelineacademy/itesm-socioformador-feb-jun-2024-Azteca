f583eed2b1c82cdae72c3a5401a8b477b6b229ac
José Carlos Sánchez <a01174050@tec.mx>
2024-06-13 16:02:46 -0600

    pruebas jose

--- test/FormTextInput.test.tsx
+++ test/FormTextInput.test.tsx
@@ -3,28 +3,34 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 describe("FormTextInput Component Test", () => {
+  // José Carlos Sánchez Gómez A01745810 - Test #1/10
   it("Should render input", () => {
     render(<FormTextInput name="name" type="text" label="Name" />);
     expect(screen.getByTestId("name")).toBeInTheDocument();
   });
+
+  // José Carlos Sánchez Gómez A01745810 - Test #2/10
   it("Should render label", () => {
     render(<FormTextInput name="name" type="text" label="Name" />);
     expect(screen.getByText("Name")).toBeInTheDocument();
   });
+
+  // José Carlos Sánchez Gómez A01745810 - Test #3/10
   it("Should render password input", () => {
     render(<FormTextInput name="password" type="password" label="Password" />);
     expect(screen.getByTestId("password")).toBeInTheDocument();
   });
+
+  // José Carlos Sánchez Gómez A01745810 - Test #4/10
   it("Should render password label", () => {
     render(<FormTextInput name="password" type="password" label="Password" />);
     expect(screen.getByText("Password")).toBeInTheDocument();
   });
-  it("Should render password show button", () => {
-    render(<FormTextInput name="password" type="password" label="Password" />);
-    expect(screen.getByText("Show")).toBeInTheDocument();
-  });
+
+  // José Carlos Sánchez Gómez A01745810 - Test #5/10
   it("Should toggle password input", () => {
     render(<FormTextInput name="password" type="password" label="Password" />);
+    expect(screen.getByText("Show")).toBeInTheDocument();
     const input = screen.getByTestId("password") as HTMLInputElement;
     const button = screen.getByText("Show");
     expect(input.type).toBe("password");
--- test/Searchbar.test.tsx
+++ test/Searchbar.test.tsx
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
 
 describe("Searchbar Component Test", () => {
+  // José Carlos Sánchez Gómez A01745810 - Test #10/10
   it("Should be rendered on the screen", () => {
     render(<SearchBar placeholder="Prueba" />);
   });
