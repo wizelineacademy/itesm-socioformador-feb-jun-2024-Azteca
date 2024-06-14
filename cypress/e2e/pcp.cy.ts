e2c39388a68ba86b10ab162b97ea4d64751363c2
José Carlos Sánchez <a01174050@tec.mx>
2024-06-14 00:53:53 -0600

    added time

--- cypress/e2e/pcp.cy.ts
+++ cypress/e2e/pcp.cy.ts
@@ -1,13 +1,14 @@
 describe("PCP", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.wait(8000);
     cy.get("label").contains("Email").click().type("cypress@outlook.com");
     cy.get("label").contains("Password").click().type("cypress");
     cy.get("button").contains("Log in").click();
-    cy.wait(12000);
+    cy.wait(15000);
     cy.url().should("include", "/dashboard");
     cy.visit("/pcp");
-    cy.wait(8000);
+    cy.wait(15000);
   });
 
   describe("PCP", () => {
