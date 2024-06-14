a0932a5dc84f24e35902ee8944312b22f7ae7324
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-06-14 01:03:14 -0600

    Added Status PCP Task Change Test

--- cypress/e2e/pcp.cy.ts
+++ cypress/e2e/pcp.cy.ts
@@ -1,13 +1,36 @@
 describe("PCP", () => {
   beforeEach(() => {
     cy.visit("/");
-    cy.wait(8000);
     cy.get("label").contains("Email").click().type("cypress@outlook.com");
     cy.get("label").contains("Password").click().type("cypress");
     cy.get("button").contains("Log in").click();
-    cy.wait(15000);
+    cy.wait(12000);
     cy.url().should("include", "/dashboard");
     cy.visit("/pcp");
-    cy.wait(15000);
+    cy.wait(8000);
+  });
+
+  describe("PCP", () => {
+    beforeEach(() => {
+      cy.visit("/");
+      cy.get("label").contains("Email").click().type("cypress@outlook.com");
+      cy.get("label").contains("Password").click().type("cypress");
+      cy.get("button").contains("Log in").click();
+      cy.wait(12000);
+      cy.url().should("include", "/dashboard");
+      cy.visit("/pcp");
+      cy.wait(8000);
+    });
+
+    it("Should change the status of the task", () => {
+      const statusButton = cy.get('[data-testid="task-status-button"]');
+      statusButton.should("exist");
+      statusButton.click();
+      cy.get('[data-testid="IN_PROGRESS"]').click();
+
+      cy.get("div")
+        .contains("Task status updated successfully")
+        .should("exist");
+    });
   });
 });
