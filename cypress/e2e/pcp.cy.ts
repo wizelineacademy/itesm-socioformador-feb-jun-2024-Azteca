e364ab69a3de2fc2c7c2b602c7927dbc82e8520f
José Carlos Sánchez <a01174050@tec.mx>
2024-06-14 01:01:58 -0600

    merged

--- cypress/e2e/pcp.cy.ts
+++ cypress/e2e/pcp.cy.ts
@@ -5,9 +5,9 @@ describe("PCP", () => {
     cy.get("label").contains("Email").click().type("cypress@outlook.com");
     cy.get("label").contains("Password").click().type("cypress");
     cy.get("button").contains("Log in").click();
-    cy.wait(12000);
+    cy.wait(15000);
     cy.url().should("include", "/dashboard");
     cy.visit("/pcp");
-    cy.wait(8000);
+    cy.wait(15000);
   });
 });
