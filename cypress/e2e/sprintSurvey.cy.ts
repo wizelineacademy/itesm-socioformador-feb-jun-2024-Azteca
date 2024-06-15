e8fdd715fc6ed9551dcac371fe51f8b0807e0258
AlexMendozaPrado <a00819383@tec.mx>
2024-06-14 22:04:45 -0600

    Prueba de Cypress Alejandro Mendoza Prado

--- cypress/e2e/sprintSurvey.cy.ts
+++ cypress/e2e/sprintSurvey.cy.ts
@@ -4,11 +4,21 @@ describe("Sprint Survey", () => {
     cy.get("label").contains("Email").click().type("cypress@outlook.com");
     cy.get("label").contains("Password").click().type("cypress");
     cy.get("button").contains("Log in").click();
-    cy.wait(12000);
+    cy.wait(15000);
     cy.url().should("include", "/dashboard");
     cy.get('[data-testid="notification-button"]').click();
     cy.get('[data-testid="Sprint Survey"]').click();
     cy.get('[data-testid="sprint-survey"]').should("exist");
+  });
+  //Alejandro Mendoza Prado A00819383
+  it("Fill step one", () => {
+    cy.get('[data-testid="sprint-step-one"]').should("exist");
+    const sliders = cy.get('[data-testid^="slider-"]');
+    sliders.each((slider) => {
+      cy.wrap(slider).find("input").invoke("val", 1).trigger("change");
+    });
+    cy.get("button").contains("Next").click();
+    cy.get('[data-testid="sprint-step-two"]').should("exist");
   });
 
   // José Eduardo de Valle Lara A01734957
